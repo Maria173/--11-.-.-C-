becf6228339f850b7cb90680707ddf0406aea44d
Mariia Shestakova <marijashestakova73@gmail.com>
2021-09-28 13:05:52 +0400

    break the application logic into classes and interfaces

---
+++ Vehicle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace WindowsFormsPlanes
+{
+    public abstract class Vehicle : ITransport
+    {
+        /// Левая координата отрисовки самолета
+        protected float _startPosX;
+
+        /// Правая кооридната отрисовки самолета
+        protected float _startPosY;
+
+        /// Ширина окна отрисовки
+        protected int _pictureWidth;
+
+        /// Высота окна отрисовки
+        protected int _pictureHeight;
+
+        /// Максимальная скорость
+        public int MaxSpeed { protected set; get; }
+
+        /// Вес самолета
+        public float Weight { protected set; get; }
+
+        /// Основной цвет
+        public Color MainColor { protected set; get; }
+        public void SetPosition(int x, int y, int width, int height)
+        {
+            _startPosX = x;
+            _startPosY = y;
+            _pictureWidth = width;
+            _pictureHeight = height;
+        }
+        public abstract void DrawTransport(Graphics g);
+        public abstract void MoveTransport(Direction direction);
+    }
+}
